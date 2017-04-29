ea819d511a8c7435b32763239ff794e4af426ead
beanstown106 <nbenis106@gmail.com>
2017-04-29 09:26:19 +0200

    Initial Pixel Gapps
    *this should only be temporary until the verity/twrp issues are fixed

---
+++ pixel-gapps-blobs.mk
@@ -0,0 +1,90 @@
+# Copyright (C) 2016 The Pure Nexus Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+PRODUCT_COPY_FILES += \
+    vendor/pixelgapps/etc/permissions/com.google.android.camera.experimental2016.xml:system/etc/permissions/com.google.android.camera.experimental2016.xml \
+    vendor/pixelgapps/etc/permissions/com.google.android.dialer.support.xml:system/etc/permissions/com.google.android.dialer.support.xml \
+    vendor/pixelgapps/etc/permissions/com.google.android.maps.xml:system/etc/permissions/com.google.android.maps.xml \
+    vendor/pixelgapps/etc/permissions/com.google.android.media.effects.xml:system/etc/permissions/com.google.android.media.effects.xml \
+    vendor/pixelgapps/etc/preferred-apps/google.xml:system/etc/preferred-apps/google.xml \
+    vendor/pixelgapps/etc/sysconfig/google.xml:system/etc/sysconfig/google.xml \
+    vendor/pixelgapps/etc/sysconfig/google_build.xml:system/etc/sysconfig/google_build.xml \
+    vendor/pixelgapps/etc/sysconfig/google_vr_build.xml:system/etc/sysconfig/google_vr_build.xml \
+    vendor/pixelgapps/etc/sysconfig/marlin_common.xml:system/etc/sysconfig/marlin_common.xml \
+    vendor/pixelgapps/etc/sysconfig/nexus.xml:system/etc/sysconfig/nexus.xml \
+    vendor/pixelgapps/etc/sysconfig/whitelist_com.android.omadm.service.xml:system/etc/sysconfig/whitelist_com.android.omadm.service.xml \
+    vendor/pixelgapps/lib/libfilterpack_facedetect.so:system/lib/libfilterpack_facedetect.so \
+    vendor/pixelgapps/lib64/libfacenet.so:system/lib64/libfacenet.so \
+    vendor/pixelgapps/lib64/libfilterpack_facedetect.so:system/lib64/libfilterpack_facedetect.so \
+    vendor/pixelgapps/priv-app/Velvet/lib/arm64/libbrotli.so:system/priv-app/Velvet/lib/arm64/libbrotli.so \
+    vendor/pixelgapps/priv-app/Velvet/lib/arm64/libcronet.so:system/priv-app/Velvet/lib/arm64/libcronet.so \
+    vendor/pixelgapps/priv-app/Velvet/lib/arm64/libframesequence.so:system/priv-app/Velvet/lib/arm64/libframesequence.so \
+    vendor/pixelgapps/priv-app/Velvet/lib/arm64/libgoogle_speech_jni.so:system/priv-app/Velvet/lib/arm64/libgoogle_speech_jni.so \
+    vendor/pixelgapps/priv-app/Velvet/lib/arm64/libgoogle_speech_micro_jni.so:system/priv-app/Velvet/lib/arm64/libgoogle_speech_micro_jni.so \
+    vendor/pixelgapps/priv-app/Velvet/lib/arm64/libnativecrashreporter.so:system/priv-app/Velvet/lib/arm64/libnativecrashreporter.so \
+    vendor/pixelgapps/priv-app/Velvet/lib/arm64/liboffline_actions_jni.so:system/priv-app/Velvet/lib/arm64/liboffline_actions_jni.so \
+    vendor/pixelgapps/priv-app/Velvet/lib/arm64/libthird_party_brotli_dec_jni.so:system/priv-app/Velvet/lib/arm64/libthird_party_brotli_dec_jni.so \
+    vendor/pixelgapps/usr/srec/en-US/APP_NAME.fst:system/usr/srec/en-US/APP_NAME.fst \
+    vendor/pixelgapps/usr/srec/en-US/APP_NAME.syms:system/usr/srec/en-US/APP_NAME.syms \
+    vendor/pixelgapps/usr/srec/en-US/c_fst:system/usr/srec/en-US/c_fst \
+    vendor/pixelgapps/usr/srec/en-US/class_normalizer.mfar:system/usr/srec/en-US/class_normalizer.mfar \
+    vendor/pixelgapps/usr/srec/en-US/CLG.prewalk.fst:system/usr/srec/en-US/CLG.prewalk.fst \
+    vendor/pixelgapps/usr/srec/en-US/commands.abnf:system/usr/srec/en-US/commands.abnf \
+    vendor/pixelgapps/usr/srec/en-US/compile_grammar.config:system/usr/srec/en-US/compile_grammar.config \
+    vendor/pixelgapps/usr/srec/en-US/config.pumpkin:system/usr/srec/en-US/config.pumpkin \
+    vendor/pixelgapps/usr/srec/en-US/confirmation_bias.fst:system/usr/srec/en-US/confirmation_bias.fst \
+    vendor/pixelgapps/usr/srec/en-US/CONTACT_NAME.fst:system/usr/srec/en-US/CONTACT_NAME.fst \
+    vendor/pixelgapps/usr/srec/en-US/CONTACT_NAME.syms:system/usr/srec/en-US/CONTACT_NAME.syms \
+    vendor/pixelgapps/usr/srec/en-US/contacts.abnf:system/usr/srec/en-US/contacts.abnf \
+    vendor/pixelgapps/usr/srec/en-US/contacts_bias.fst:system/usr/srec/en-US/contacts_bias.fst \
+    vendor/pixelgapps/usr/srec/en-US/contacts_disambig.fst:system/usr/srec/en-US/contacts_disambig.fst \
+    vendor/pixelgapps/usr/srec/en-US/dict:system/usr/srec/en-US/dict \
+    vendor/pixelgapps/usr/srec/en-US/dictation.config:system/usr/srec/en-US/dictation.config \
+    vendor/pixelgapps/usr/srec/en-US/dnn:system/usr/srec/en-US/dnn \
+    vendor/pixelgapps/usr/srec/en-US/endpointer_dictation.config:system/usr/srec/en-US/endpointer_dictation.config \
+    vendor/pixelgapps/usr/srec/en-US/endpointer_model:system/usr/srec/en-US/endpointer_model \
+    vendor/pixelgapps/usr/srec/en-US/endpointer_model.mmap:system/usr/srec/en-US/endpointer_model.mmap \
+    vendor/pixelgapps/usr/srec/en-US/endpointer_voicesearch.config:system/usr/srec/en-US/endpointer_voicesearch.config \
+    vendor/pixelgapps/usr/srec/en-US/ep_portable_mean_stddev:system/usr/srec/en-US/ep_portable_mean_stddev \
+    vendor/pixelgapps/usr/srec/en-US/ep_portable_model.mmap:system/usr/srec/en-US/ep_portable_model.mmap \
+    vendor/pixelgapps/usr/srec/en-US/g2p.data:system/usr/srec/en-US/g2p.data \
+    vendor/pixelgapps/usr/srec/en-US/g2p_fst:system/usr/srec/en-US/g2p_fst \
+    vendor/pixelgapps/usr/srec/en-US/grammar.config:system/usr/srec/en-US/grammar.config \
+    vendor/pixelgapps/usr/srec/en-US/graphemes.syms:system/usr/srec/en-US/graphemes.syms \
+    vendor/pixelgapps/usr/srec/en-US/hmmlist:system/usr/srec/en-US/hmmlist \
+    vendor/pixelgapps/usr/srec/en-US/hmm_symbols:system/usr/srec/en-US/hmm_symbols \
+    vendor/pixelgapps/usr/srec/en-US/input_mean_std_dev:system/usr/srec/en-US/input_mean_std_dev \
+    vendor/pixelgapps/usr/srec/en-US/lexicon.U.fst:system/usr/srec/en-US/lexicon.U.fst \
+    vendor/pixelgapps/usr/srec/en-US/lstm_model.uint8.data:system/usr/srec/en-US/lstm_model.uint8.data \
+    vendor/pixelgapps/usr/srec/en-US/magic_mic.config:system/usr/srec/en-US/magic_mic.config \
+    vendor/pixelgapps/usr/srec/en-US/metadata:system/usr/srec/en-US/metadata \
+    vendor/pixelgapps/usr/srec/en-US/monastery_config.pumpkin:system/usr/srec/en-US/monastery_config.pumpkin \
+    vendor/pixelgapps/usr/srec/en-US/normalizer.mfar:system/usr/srec/en-US/normalizer.mfar \
+    vendor/pixelgapps/usr/srec/en-US/norm_fst:system/usr/srec/en-US/norm_fst \
+    vendor/pixelgapps/usr/srec/en-US/offensive_word_normalizer.mfar:system/usr/srec/en-US/offensive_word_normalizer.mfar \
+    vendor/pixelgapps/usr/srec/en-US/offline_action_data.pb:system/usr/srec/en-US/offline_action_data.pb \
+    vendor/pixelgapps/usr/srec/en-US/phonelist:system/usr/srec/en-US/phonelist \
+    vendor/pixelgapps/usr/srec/en-US/phonelist.syms:system/usr/srec/en-US/phonelist.syms \
+    vendor/pixelgapps/usr/srec/en-US/phonemes.syms:system/usr/srec/en-US/phonemes.syms \
+    vendor/pixelgapps/usr/srec/en-US/portable_lstm:system/usr/srec/en-US/portable_lstm \
+    vendor/pixelgapps/usr/srec/en-US/portable_meanstddev:system/usr/srec/en-US/portable_meanstddev \
+    vendor/pixelgapps/usr/srec/en-US/pumpkin.mmap:system/usr/srec/en-US/pumpkin.mmap \
+    vendor/pixelgapps/usr/srec/en-US/rescoring.fst.louds:system/usr/srec/en-US/rescoring.fst.louds \
+    vendor/pixelgapps/usr/srec/en-US/semantics.pumpkin:system/usr/srec/en-US/semantics.pumpkin \
+    vendor/pixelgapps/usr/srec/en-US/SONG_NAME.fst:system/usr/srec/en-US/SONG_NAME.fst \
+    vendor/pixelgapps/usr/srec/en-US/SONG_NAME.syms:system/usr/srec/en-US/SONG_NAME.syms \
+    vendor/pixelgapps/usr/srec/en-US/voice_actions.config:system/usr/srec/en-US/voice_actions.config \
+    vendor/pixelgapps/usr/srec/en-US/voice_actions_compiler.config:system/usr/srec/en-US/voice_actions_compiler.config \
+    vendor/pixelgapps/usr/srec/en-US/word_classifier:system/usr/srec/en-US/word_classifier \
+    vendor/pixelgapps/usr/srec/en-US/wordlist.syms:system/usr/srec/en-US/wordlist.syms
---
+++ pixel-gapps.mk
@@ -0,0 +1,63 @@
+# Copyright (C) 2016 The Pure Nexus Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+#gapps blobs
+$(call inherit-product, vendor/pixelgapps/pixel-gapps-blobs.mk)
+
+# /app
+PRODUCT_PACKAGES += \
+    CalendarGooglePrebuilt \
+    Chrome \
+    FaceLock \
+    GoogleCamera \
+    GoogleContactsSyncAdapter \
+    GoogleTTS \
+    GoogleVrCore \
+    Hangouts \
+    PartnerBookmarksProvider \
+    Photos \
+    PlayAutoInstallConfig \
+    PrebuiltBugle \
+    PrebuiltDeskClockGoogle \
+    talkback
+
+# /framework
+PRODUCT_PACKAGES += \
+    com.google.android.camera.experimental2016 \
+    com.google.android.dialer.support \
+    com.google.android.maps \
+    com.google.android.media.effects
+
+# /priv-app
+PRODUCT_PACKAGES += \
+    ConfigUpdater \
+    ConnMetrics \
+    GmsCoreSetupPrebuilt \
+    GoogleBackupTransport \
+    GoogleFeedback \
+    GoogleLoginService \
+    GoogleOneTimeInitializer \
+    GooglePartnerSetup \
+    GoogleServicesFramework \
+    Phonesky \
+    PrebuiltGmsCore \
+    DynamiteModulesA \
+    DynamiteModulesB \
+    DynamiteModulesC \
+    SetupWizard \
+    Velvet
+
+# /symlinks
+PRODUCT_PACKAGES += \
+    libfacenet.so
